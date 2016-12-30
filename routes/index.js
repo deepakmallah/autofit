8fe768590692456b602ee381309228ec65111298
Deepak M <deepak.mallah@rentomojo.com>
2016-12-30 19:27:19 +0530

    ImageMagic -> Basic working version

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
+var path = require('path');
+var imagemagick = require('imagemagick-native');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  fs.writeFileSync(path.resolve('./public/images/after.png'), imagemagick.convert({
+    srcData: fs.readFileSync(path.resolve('./public/images/before.png')),
+    format: 'PNG',
+    quality: 100 // (best) to 1 (worst)
+  }));
+
   res.render('index', { title: 'Express' });
 });
 
