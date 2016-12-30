d7d7f6866c71e47900a9229a788330ba77095624
Deepak M <deepak.mallah@rentomojo.com>
2016-12-30 20:14:50 +0530

    version v2

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,25 @@ var imagemagick = require('imagemagick-native');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  fs.writeFileSync(path.resolve('./public/images/after.png'), imagemagick.convert({
-    srcData: fs.readFileSync(path.resolve('./public/images/before.png')),
+  var options = {
+    srcData: fs.readFileSync(path.resolve('./public/images/before.jpg')),
     format: 'PNG',
-    quality: 100 // (best) to 1 (worst)
-  }));
+    quality: 100, // (best) to 1 (worst)
+    width: 200,
+    height: 200,
+    caption: "jhvjhvhvhuvv bjbj  jbjbjbj",
+    text: "Deepak"
+  };
 
-  res.render('index', { title: 'Express' });
+  imagemagick.convert(options, function (err, buffer) {
+    console.log("Err")
+    console.log(err)
+    console.log("<br />")
+    console.log("success")
+    console.log(buffer)
+    fs.writeFileSync(path.resolve('./public/images/after.png'), buffer);
+
+  });
 });
 
 module.exports = router;
