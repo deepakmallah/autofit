a0b47fedd5286e5b62e79d500753b3b6cb7abcf5
Deepak M <deepak.mallah@rentomojo.com>
2017-01-01 15:43:37 +0530

    Completed

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,128 @@
 var express = require('express');
 var server = express()
 var router = express.Router();
-var fs = require('fs');
 var path = require('path');
+var fs = require('fs');
 var gm = require('gm').subClass({ imageMagick: true });
 var currentTimeStamp = Math.floor(new Date() / 1000);
 var im = require('imagemagick');
+var _dir = "public/images/";
+var name = null;
+var baseImage = "public/images/default.jpg";
+var finalImage = _dir+'resulted_file.jpg'
+
 /**
- * Creates New file name using current Timestamp
- * @param name
- * @returns {string}
+ * ////return new Promise(function(resolve, reject){
+ * //gm(request(url))
+ * //http://aheckmann.github.io/gm/docs.html
  */
-function getNewFileName(name){
-  var tmp = name.split(".");
-  return tmp[0]+"_"+currentTimeStamp+"."+tmp[1];
-};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  var file = path.basename('./public/images/before.jpg');
-  var fileName = getNewFileName(file);
-  console.log(fileName);
-
-  ////return new Promise(function(resolve, reject){
-  //gm(request(url))
-  //http://aheckmann.github.io/gm/docs.html
-  var msg = "A";
-  var args = [
-    '-strokewidth','1',
-    '-stroke','red',
-    '-background','yellow',
-    '-fill','white',
-    '-gravity','center',
-    '-size',200+'x'+200,
-    "caption:"+unescape(msg),
-    path.resolve('./public/images/before_new.jpg'),
-  ];
-  im.convert(args, function(err, response){
-    if(err) console.log(err)
-
-    gm()
-      .command("composite")
-      .in("-geometry", "+100+100")
-      .in(path.resolve('./public/images/before_new.jpg'))
-      .in(path.resolve('./public/images/before.jpg'))
-      .write(path.resolve('./public/images/resulted_file.jpg'), function (err) {
-        if (err)
-          console.log(err)
-        else
-          res.render('index', { title: path.resolve('./public/images/resulted_file.jpg') });
-      });
-  });
+  res.render('index', { title: "Autofit Text in the Box", img: finalImage});
 });
 
 router.post('/generate', function(req, res, next) {
-  console.log("res")
-  console.log(res)
-
-  console.log("req")
-  console.log(req)
-
+  if(req && req.body){
+    generateImg(req.body.str)
+      .then(function(err, response){
+        return res.redirect('/');
+      })
+      .catch(function(err){
+        console.log(err)
+        next();
+      });
+  }
 });
+
+/**
+ * random string generator from alphanumeric and current Timestamp
+ * @param name
+ * @returns {string}
+ */
+function getName(str){
+  return new Promise(function(resolve, reject){
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 5; i++ ){
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    this.name = text;
+    return resolve(str);
+  });
+};
+
+/**
+ * Creates text in the box
+ * @param str
+ * @returns {Promise}
+ */
+var textInBox = function(str){
+  return new Promise(function(resolve, reject){
+    if(str){
+      try{
+        var fileName = _dir+this.name+'.png';
+        var args = [
+          '-border','1%x1%',
+          //'-strokewidth','1',
+          //'-stroke','black',
+          '-background','none',
+          '-fill','blue',
+          '-gravity','center',
+          '-size',200+'x'+200,
+          //'-extent', 200+'x'+200,
+          "caption:"+unescape(str),
+          path.resolve(fileName),
+        ];
+        im.convert(args, function(err, response){
+          if (err)
+            return reject({error: true, msg: err});
+          else
+            return resolve(fileName);
+        });
+      }catch (err){
+        return reject({error: true, msg: err});
+      }
+    }else{
+      return reject({error: true, msg: "string is missing."});
+    }
+  });
+};
+
+var mergeImage = function(boxedImg){
+  return new Promise(function(resolve, reject){
+    try{
+      gm()
+        .command("composite")
+        .in("-geometry", "+100+100")
+        .in(path.resolve(boxedImg))
+        .in(path.resolve(baseImage))
+        .write(path.resolve(finalImage), function (err) {
+          if (err)
+            return reject({error: true, msg: err});
+          else
+            fs.unlink(boxedImg);
+            return resolve(finalImage);
+        });
+    }catch (err){
+      return reject({error: true, msg: err});
+    }
+  });
+};
+
+var generateImg = function(str){
+  return new Promise(function(resolve, reject){
+    getName(str)
+      .then(textInBox)
+      .then(mergeImage)
+      .then(function(err, res){
+        return resolve(res);
+      })
+      .catch(function(err){
+        console.log("Error Occured while processing => "+err.msg);
+      });
+  });
+};
 
 module.exports = router;
